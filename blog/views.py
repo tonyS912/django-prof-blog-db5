4949d6dc18cb918de976ebb9ef1947ea0c8ea4d7
Tonys <info@tony-schiller.com>
2025-11-06 17:15:37 +0100

    update blog app, add first urls

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 
@@ -8,6 +8,7 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     try:
         post = Post.published.get(id=id)
     except Post.DoesNotExist:

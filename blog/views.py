c8cb9663c674bd10ad63fd49363d4f5e5902b3c2
Tonys <info@tony-schiller.com>
2025-11-17 13:42:40 +0100

    implement forms.py and update views and urls

--- blog/views.py
+++ blog/views.py
@@ -6,21 +6,15 @@ from .models import Post
 
 # Create your views here.
 class PostListView(ListView):
-    """_summary_
-
-    Args:
-        ListView (_type_): _description_
-    """
-
     queryset = (
         Post.published.all()
     )  # Custom QuerySet like model = Post, django would built the generic Post.obects.all() QuerSet
-    context_object_name = "post"  # if we don't specify special name with context_object_name for the query results, default will be object_list
+    context_object_name = "posts"  # if we don't specify special name with context_object_name for the query results, default will be object_list
     paginate_by = 3  # how much objects will be returned per Page
     template_name = "blog/post/list.html"  # using custom template to render, default django would search for blog/post_list.html
 
 
-def post_list(request):
+""" def post_list(request):
     post_list = Post.published.all()
     # Pagination with 3 post per page
     paginator = Paginator(post_list, 3)
@@ -35,7 +29,7 @@ def post_list(request):
         posts = paginator.page(1)
 
     # Render Post List
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts}) """
 
 
 def post_detail(request, year, month, day, post):

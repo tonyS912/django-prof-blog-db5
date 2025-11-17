3fef2df87d568d9ce97709901e052d6753a0fd6e
Tonys <info@tony-schiller.com>
2025-11-17 16:24:47 +0100

    update forms and views for sharing posts

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    name = forms.CharField(max_length=25)  # Name who send the mail
+    email = forms.EmailField()  # E-Mail Address sending from
+    to = forms.EmailField()  # E-Mail Address sending to
+    comments = forms.CharField(
+        required=False, widget=forms.Textarea
+    )  # Text how to send
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 
 
 # Create your views here.
@@ -42,3 +43,18 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == "POST":
+        # Form was Submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send mail
+    else:
+        form = EmailPostForm()
+    return render(request, "blog/post/share.html", {"post": post, "form": form})

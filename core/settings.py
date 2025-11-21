16df027079c9be80c45cbb9c310f922a0ee5f98b
Tonys <info@tony-schiller.com>
2025-11-21 09:10:44 +0100

    update settings for messaging

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,8 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Email Server configuration
-EMAIL_HOST = ""
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", "localhost")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587

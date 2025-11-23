7d9fa8ca4b7e81a3073f9d0be3956c014fa03ecf
Tonys <info@tony-schiller.com>
2025-11-23 11:45:31 +0100

    update send-mail view, upadate url to share posts, add ne django-template for sharing

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,15 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    # Post views
-    # path("", views.post_list, name="post_list"),
+    # ^ Post views
+    # List view
     path("", views.PostListView.as_view(), name="post_list"),
+    # Detail view
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/",
         views.post_detail,
         name="post_detail",
     ),
+    # Share views
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import EmailPostForm
@@ -48,13 +49,36 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    # Initiate variable to False
+    sent = False
     if request.method == "POST":
         # Form was Submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send mail
+            # ^ ... send mail Process
+            # initiate the absolute path for the post we want to share
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            # create Subject Body
+            subject = (
+                f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
+            )
+            # create Message Body
+            message = (
+                f"Read {post.title} at {post_url} \n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            # send mail
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, "blog/post/share.html", {"post": post, "form": form})
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
